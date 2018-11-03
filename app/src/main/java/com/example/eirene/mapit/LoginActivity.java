5523f129cb2d85dec7de1877f45b5467cb131702
Michael Sihombing <michael21sihombing@gmail.com>
2018-11-03 22:40:47 +0700

    Melakukan penambahan aktivitas Login dimana akan menampilkan menu Login dengan email dan password pada MAPIT.
    Aktifitas ini adalah aktifitas yang akan ditampilkan setelah SplashActivity.
    
    Pada aktifitas ini untuk sementara email "user" password "123"

---
+++ app/src/main/java/com/example/eirene/mapit/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.example.eirene.mapit;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText userPassword;
+    private Button buttonLogin;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userName = (EditText)findViewById(R.id.mitUsername);
+        userPassword = (EditText)findViewById(R.id.mitPassword);
+        buttonLogin = (Button)findViewById(R.id.btnLogin);
+        userRegistration = (TextView)findViewById(R.id.mitSignUp);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(userName.getText().toString(), userPassword.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+
+        progressDialog.setMessage("Loading . . .");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    //Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    checkEmailVerification();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+        });
+
+    }
+
+    private void checkEmailVerification(){
+        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        Boolean emailflag = firebaseUser.isEmailVerified();
+
+        if(emailflag){
+            finish();
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }else{
+            Toast.makeText(this, "Verify your email", Toast.LENGTH_LONG).show();
+            firebaseAuth.signOut();
+        }
+    }
+
+}
